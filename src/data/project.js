914ee45817f772618e66aaa15598b750622b8476
Rahulwagh07 <121787635+Rahulwagh07@users.noreply.github.com>
2023-09-01 18:51:39 +0530

    Updated project.js

--- src/data/project.js
+++ src/data/project.js
@@ -7,25 +7,26 @@ import  Topcourses from "../assets/Videos/Topcourses.mp4"
 export  const projectData = [
  {
     title : "Studynotion- Edtech Platform",
-    info : "I apologize for the confusion. If the previous approaches are not working as expected, there might be some other conflicting styles affecting the layout.",
+    info : "StudyNotion is an  ed-tech platform that enables users to create, consume, and rate educational content. The platform is built using the MERN stack, which includes ReactJS, NodeJS, MongoDB, and ExpressJS.
+",
     video: Studynotion,
     link: "https://studynotion-fronted-nine.vercel.app/",
  },
  {
     title : "Random Gif-Generator",
-    info : "I apologize for the confusion. If the previous approaches are not working as expected, there might be some other conflicting styles affecting the layout.",
+    info : "This project is a random GIF generator built with React.js using a custom hook. It allows users to generate and display random GIFs on a web page. It uses the GIPHY API.",
     video: RandomGif,
     link: "https://random-gif-generator-mu.vercel.app/",
  },
  {
     title : "SmartCart",
-    info : "I apologize for the confusion. If the previous approaches are not working as expected, there might be some other conflicting styles affecting the layout.",
+    info : "This is a shopping cart application built using Redux Toolkit. It demonstrates the usage of Redux Toolkit to manage the state of a shopping cart in a React applicationt",
     video: Shoppingcart,
     link: "https://shooping-cart-roan.vercel.app/",
  },
  {
    title : "Top Course Filter",
-   info : "I apologize for the confusion. If the previous approaches are not working as expected, there might be some other conflicting styles affecting the layout.",
+   info : "This is a React Courses Project that displays a list of courses fetched from an API. It uses useEffect react hook to fetch data from API.",
    video: Topcourses,
    link: "https://top-courses-virid.vercel.app/",
 },
